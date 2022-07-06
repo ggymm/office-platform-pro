6fcb182733a7b31dffd220ce2d5e50739eb5eacb
gongym <1967988842@qq.com>
2022-07-06 17:57:46 +0800

    格式化代码

--- app/src/app.vue
+++ app/src/app.vue
@@ -5,15 +5,7 @@
 </template>
 
 <script setup>
-import { useRouter, useRoute } from 'vue-router'
 import Layout from '~/layout/index.vue'
-
-const router = useRouter()
-const route = useRoute()
-
-function test() {
-  router.push({ path: '/message' })
-}
 
 </script>
 
